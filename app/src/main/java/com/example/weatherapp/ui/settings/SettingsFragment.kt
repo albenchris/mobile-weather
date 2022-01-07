0958af2430351f065bc0c660cb9b7015907ca5bf
albenchris <albenchris00@gmail.com>
2022-01-07 12:05:21 -0600

    bottom navigation bar now navigates between fragments

--- app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
@@ -1,6 +1,6 @@
 package com.example.weatherapp.ui.settings
 
-import android.preference.PreferenceFragment
+//import android.preference.PreferenceFragment
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment() {
